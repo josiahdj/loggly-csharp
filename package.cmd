73cff90ba61de97b090cdfa56cf43b2f6addd869
Josiah Johnson <josiahdj@gmail.com>
2013-12-24 18:23:21 -0500

    going back down to .net 40 (not all projects support 4.5 yet)

--- package.cmd
+++ package.cmd
@@ -1,7 +1,7 @@
 mkdir NuGet.Packager\lib
-mkdir NuGet.Packager\lib\net45
+mkdir NuGet.Packager\lib\net40
 mkdir NuGet.Packager\content
 mkdir NuGet.Packager\src
 
-copy loggly-csharp\bin\Release\Loggly.dll NuGet.Packager\lib\net45\
+copy loggly-csharp\bin\Release\Loggly.dll NuGet.Packager\lib\net40\
 
