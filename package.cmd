288f459a66087ffefd129207c40a98168b4e2e7f
Josiah Johnson <josiahdj@gmail.com>
2013-12-24 17:50:37 -0500

    making sure the target directories for nuget exist before the nuget packager is populated and run

--- package.cmd
+++ package.cmd
@@ -1,2 +1,7 @@
+mkdir NuGet.Packager\lib
+mkdir NuGet.Packager\lib\net45
+mkdir NuGet.Packager\content
+mkdir NuGet.Packager\src
+
 copy loggly-csharp\bin\Release\Loggly.dll NuGet.Packager\lib\net45\
 
